bb199e2cb8650c0ef0fdd3393e67dc00cd225656
Nisa <nisameshal@gmail.com>
2022-11-16 02:48:33 -0800

    recycler view and adapter for parking spots

---
+++ app/src/main/java/com/app/parking/SpotListActivity.kt
@@ -0,0 +1,28 @@
+package com.app.parking
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+
+/*
+This is where we bind the data we got from the adapter to the recycler view
+ */
+class SpotListActivity : AppCompatActivity(){
+    lateinit var spots : ArrayList<Spot>
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        //connect to the recycler view
+        val rvSpots = findViewById<View>(R.id.spots) as RecyclerView
+        //make list of 5
+        //TODO populate spots with query data
+        spots = Spot.createParkingList(15)
+        //instantiate adapter and pass in the spots data
+        val adapter = SpotAdapter(spots)
+        rvSpots.adapter = adapter
+        rvSpots.layoutManager = LinearLayoutManager(this)
+    }
+}

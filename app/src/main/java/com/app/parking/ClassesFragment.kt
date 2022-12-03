bbeca66d866f766dd59b823026057779d8e908a5
Nisa Meshal <nisameshal@gmail.com>
2022-12-03 14:20:37 -0800

    class fragment

--- app/src/main/java/com/app/parking/ClassesFragment.kt
+++ app/src/main/java/com/app/parking/ClassesFragment.kt
@@ -5,13 +5,14 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 
 
 class ClassesFragment : Fragment() {
+    var classes : ArrayList<Class> = ArrayList<Class>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
     }
 
     override fun onCreateView(
@@ -20,7 +21,10 @@ class ClassesFragment : Fragment() {
     ): View? {
         val rootView = inflater.inflate(R.layout.fragment_classes, container, false)
 
-        val onClickListener = View.OnClickListener { itemView : View ->
+        val onClickListener = View.OnClickListener { itemView: View ->
+            val selectClassId = itemView.tag as Int
+            val args = Bundle()
+            args.putInt(ARG_CLASS_ID, selectClassId)
 
         }
 
--- app/src/main/java/com/app/parking/Task.kt
+++ app/src/main/java/com/app/parking/Task.kt
@@ -4,7 +4,7 @@ import java.time.LocalDate
 import java.util.Date
 
 data class Task (
-    var id: Int = 0,
     var name: String = "",
-    var dueDate : LocalDate = LocalDate.now() ){
+    var dueDate : LocalDate = LocalDate.now(),
+    var notes: String = ""){
 }

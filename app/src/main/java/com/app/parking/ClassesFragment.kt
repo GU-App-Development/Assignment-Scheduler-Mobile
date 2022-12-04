75feb6a04fa9cf3235558fbeced7179cc9f29d2f
Jonathan <jsmoley@zagmail.gonzaga.edu>
2022-12-03 17:34:53 -0800

    band database adapter code supplemented

--- app/src/main/java/com/app/parking/ClassesFragment.kt
+++ app/src/main/java/com/app/parking/ClassesFragment.kt
@@ -4,16 +4,16 @@ import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
+import android.view.View.OnClickListener
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.TextView
+import androidx.navigation.Navigation
+import androidx.recyclerview.widget.RecyclerView
 
 
 class ClassesFragment : Fragment() {
-    var classes : ArrayList<Class> = ArrayList<Class>()
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
+    private var classes : ArrayList<Class> = ArrayList()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -26,9 +26,48 @@ class ClassesFragment : Fragment() {
             val args = Bundle()
             args.putInt(ARG_CLASS_ID, selectClassId)
 
+            // class details
+
+            Navigation.findNavController(itemView).navigate(R.id.action_class_to_task, args)
         }
+
+        val recyclerView = rootView.findViewById<RecyclerView>(R.id.class_recycler)
+        classes = ClassRepository.getInstance(requireContext()).classList
+        recyclerView.adapter = ClassAdapter(classes, onClickListener)
 
         return rootView
     }
+    private class ClassAdapter(private val classList: List<Class>,
+                               private val onClickListener: View.OnClickListener)
+        : RecyclerView.Adapter<ClassHolder>() {
 
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) : ClassHolder {
+            val layoutInflater = LayoutInflater.from(parent.context)
+            return ClassHolder(layoutInflater, parent)
+        }
+
+        override fun onBindViewHolder(holder: ClassHolder, position: Int) {
+            val aClass = classList[position]
+            holder.bind(aClass)
+            holder.itemView.tag = aClass.id
+            holder.itemView.setOnClickListener(onClickListener)
+        }
+
+        override fun getItemCount(): Int {
+            return classList.size
+        }
+    }
+
+    private class ClassHolder(inflater: LayoutInflater, parent: ViewGroup?) :
+        RecyclerView.ViewHolder(inflater.inflate(R.layout.list_item_assignment, parent, false)) {
+            private val nameTextView: TextView
+
+            init {
+                nameTextView = itemView.findViewById(R.id.class_name)
+            }
+
+            fun bind(aClass: Class) {
+                nameTextView.text = aClass.name
+            }
+        }
 }
